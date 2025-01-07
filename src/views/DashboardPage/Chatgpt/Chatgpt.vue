9f95781e1e86272b7c84f2bed411c9c5c02bea77
Jamshid <jamshidabdukarimov776@gmail.com>
2025-01-07 07:31:47 -0800

    fix netlify

--- src/views/DashboardPage/Chatgpt/Chatgpt.vue
+++ src/views/DashboardPage/Chatgpt/Chatgpt.vue
@@ -105,7 +105,6 @@ async function getLiamaResponse(userInput) {
         }
       }
     );
-    console.log("Liama API Response:", response.data);
     const botMessage = response.data.choices[0].message.content;
     chatMessages.value[store.selectedItem].push({
       type: "bot",
@@ -208,7 +207,6 @@ onMounted(() => {
 });
 </script>
 
-
 <template>
   <section>
     <div v-if="store.selectedItem === 'chatgpt'" class="chatall">
@@ -249,7 +247,7 @@ onMounted(() => {
         <div class="chat-body">
           <div
             class="message-container"
-            v-for="(msg, index) in chatMessages.claude"
+            v-for="(msg, index) in chatMessages.liama"
             :key="index"
           >
             <div :class="['message', msg.type]">
@@ -287,17 +285,7 @@ onMounted(() => {
               <div v-if="msg.image">
                 <div class="download-wr">
                   <button class="download" @click="downloadImage(msg.image)">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="32"
-                      height="32"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.712T12 4t.713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062M6 20q-.825 0-1.412-.587T4 18v-2q0-.425.288-.712T5 15t.713.288T6 16v2h12v-2q0-.425.288-.712T19 15t.713.288T20 16v2q0 .825-.587 1.413T18 20z"
-                      />
-                    </svg>
+                    Download
                   </button>
                 </div>
                 <img :src="msg.image" alt="Generated image" />
@@ -376,6 +364,5 @@ onMounted(() => {
         </div>
       </div>
     </div>
-    
   </section>
 </template>
